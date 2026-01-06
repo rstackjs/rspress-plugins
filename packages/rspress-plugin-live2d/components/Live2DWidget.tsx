42e4920dfc3ea4fdcb279f0f0ae40a37cc98c7df
SoonIter <sooniter@gmail.com>
2026-01-07 05:03:45 +0800

    refactor: live-2d

--- packages/rspress-plugin-live2d/components/Live2DWidget.tsx
+++ packages/rspress-plugin-live2d/components/Live2DWidget.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 
-import { loadOml2d } from 'oh-my-live2d';
-
 import type { Live2DWidgetProps } from '../src/typings';
 
 const Live2DWidget: React.FC<Live2DWidgetProps> = (options) => {
   useEffect(() => {
-    loadOml2d(options);
+    import('oh-my-live2d').then(({ loadOml2d }) => {
+      console.log(1111111);
+      loadOml2d(options);
+    });
   }, []);
 
   return null;
