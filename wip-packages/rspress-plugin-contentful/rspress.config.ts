5d1e426a5ec114530769d581cee328cfcea07194
SoonIter <sooniter@gmail.com>
2026-01-07 05:13:13 +0800

    chore: remove some pkgs

--- wip-packages/rspress-plugin-contentful/rspress.config.ts
+++
@@ -1,9 +0,0 @@
-import * as path from 'path';
-import { defineConfig } from '@rspress/core';
-import contentful from './src';
-
-export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  title: 'Rspress x Contentful Example',
-  plugins: [contentful()],
-});
--- wip-packages/rspress-plugin-graphql-playground/rspress.config.ts
+++
@@ -1,9 +0,0 @@
-import * as path from 'path';
-import { defineConfig } from '@rspress/core';
-import graphql from './src';
-
-export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  title: 'Rspress x GraphQL Playground Example',
-  plugins: [graphql()],
-});
