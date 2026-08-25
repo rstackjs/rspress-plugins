dbf27335dbe4630db04b9de32b7e6126a49f8678
neverland <chenjiahan.jait@bytedance.com>
2026-08-25 16:15:01 +0800

    refactor(file-tree): use import.meta.dirname

--- packages/rspress-plugin-file-tree/src/index.ts
+++ packages/rspress-plugin-file-tree/src/index.ts
@@ -1,5 +1,4 @@
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 import { RemarkCodeBlockToGlobalComponentPluginFactory } from 'rspress-plugin-devkit';
 
@@ -11,10 +10,7 @@ interface RspressPluginFileTreeOptions {
   initialExpandDepth?: number;
 }
 
-const PACKAGE_ROOT = path.resolve(
-  path.dirname(fileURLToPath(import.meta.url)),
-  '..',
-);
+const PACKAGE_ROOT = path.resolve(import.meta.dirname, '..');
 
 export default function rspressPluginFileTree(
   options: RspressPluginFileTreeOptions = {},
